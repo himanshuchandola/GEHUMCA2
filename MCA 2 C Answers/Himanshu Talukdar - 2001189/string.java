866f5bf1a6036b5cb39e54a2f0d9aece116f74c4
HimanshuTalukdar <85876894+HimanshuTalukdar@users.noreply.github.com>
2021-06-22 12:00:55 +0530

    Add files via upload

---
+++ MCA 2 C Answers/Himanshu Talukdar - 2001189/string.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;  
+public string
+{
+
+	 public static void main(String[] args) 
+	 {
+
+	  String str1 = "Himanshu";
+	  String d= "";
+
+	  for (int i = str1.length() - 1; i >= 0; --i) {
+	   d += str1.charAt(i);
+	  }
+
+
+	  char[] str=d.toCharArray();
+	  for(int i=0;i<d.length();i++)
+	     {
+	  if(i==0 || str[i-1]==' ')
+	     {
+	      str[i]=Character.toUpperCase(str[i]);
+	     }
+	   else if(str[i]==' ' || str[i]=='\0')
+	       str[i-1]=Character.toUpperCase(str[i-1]);
+	     }
+	  System.out.print("After Converting String is: ");
+	  for(int i=0;i<d.length();i++)
+	  System.out.print(str[i]);
+	 	 }
+	}
